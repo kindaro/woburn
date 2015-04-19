d586d20e5a9a1648b919ad0b4eaae3228d6a1e3d
Sivert Berg <code@trev.is>
2015-04-19 10:44:50 +0200

    Added Backend data to Surface

--- src/Woburn/Surface.hs
+++ src/Woburn/Surface.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Woburn.Surface
     ( SurfaceMonad (..)
     , SurfaceId
@@ -26,8 +28,9 @@ import Linear
 import Graphics.Wayland
 import Woburn.Protocol
 
-class Monad m => SurfaceMonad m where
-    surfCommit  :: Surface -> m ()
+class Monad m => SurfaceMonad m s | m -> s where
+    surfCommit :: Surface s -> m ()
+    surfCreate :: m s
 
 data Buffer = Buffer
 
@@ -54,7 +57,7 @@ data SurfaceState =
                  , surfTransform    :: WlOutputTransform
                  }
 
-data Surface =
+data Surface s =
     Surface { surfId            :: SurfaceId             -- ^ Unique ID.
             , surfSync          :: Bool                  -- ^ Whether the surface is in sync mode.
             , surfDeleted       :: Bool                  -- ^ Whether the surface has been deleted.
@@ -68,15 +71,16 @@ data Surface =
                                                          --   this is copied from the current state when the parent
                                                          --   is committed, otherwise it is updated at the same time
                                                          --   as the current state.
+            , surfBackendData   :: s                     -- ^ Internal data used by the backend.
             }
 
-instance Eq Surface where
+instance Eq (Surface s) where
     (==) = (==) `on` surfId
 
-instance Ord Surface where
+instance Ord (Surface s) where
     compare = comparing surfId
 
-type SurfaceSet = STree Surface
+type SurfaceSet s = STree (Surface s)
 
 -- | The initial state given to a new surface.
 initialState :: SurfaceState
@@ -105,28 +109,31 @@ flipBuffered f b = Buffered { currentState = pendingState b
                             }
 
 -- | Creates a new surface.
-newSurface :: Surface
-newSurface = Surface { surfId            = undefined
-                     , surfSync          = False
-                     , surfDeleted       = False
-                     , surfNewState      = False
-                     , surfState         = Buffered initialState initialState
-                     , surfPosition      = Buffered 0 0
-                     , surfFrameCallback = []
-                     , surfRole          = Nothing
-                     , surfShuffle       = []
-                     , surfCurInput      = everything
-                     }
+newSurface :: SurfaceMonad m s => m (Surface s)
+newSurface = do
+    s <- surfCreate
+    return Surface { surfId            = undefined
+                   , surfSync          = False
+                   , surfDeleted       = False
+                   , surfNewState      = False
+                   , surfState         = Buffered initialState initialState
+                   , surfPosition      = Buffered 0 0
+                   , surfFrameCallback = []
+                   , surfRole          = Nothing
+                   , surfShuffle       = []
+                   , surfCurInput      = everything
+                   , surfBackendData   = s
+                   }
 
 -- | Commits the pending state to the current state.
-commitState :: Surface -> Surface
+commitState :: Surface s -> Surface s
 commitState surf = surf { surfState    = flipBuffered nextState (surfState surf)
                         , surfNewState = True
                         }
 
 -- | Commits the surface to the backend, and updates surface position and input
 -- region.
-commitBackend :: SurfaceMonad m => Surface -> m Surface
+commitBackend :: SurfaceMonad m s => Surface s -> m (Surface s)
 commitBackend surf
     | not (surfNewState surf) = return surf
     | otherwise               = do
@@ -138,28 +145,28 @@ commitBackend surf
         return surf'
 
 -- | Applies all the shuffles in 'surfShuffle'.
-shuffle :: SurfaceSet -> SurfaceSet
+shuffle :: SurfaceSet s -> SurfaceSet s
 shuffle = id
 
 -- | Prunes the subtree of any child-surfaces that has been deleted.
-prune :: SurfaceSet -> SurfaceSet
+prune :: SurfaceSet s -> SurfaceSet s
 prune = id
 
 -- | Modifies a SurfceSet by applying functions to its branches and node.
 modifySet :: (Monad m, Applicative m)
-          => (SurfaceSet -> m SurfaceSet)
-          -> (Surface -> m Surface)
-          -> SurfaceSet
-          -> m SurfaceSet
+          => (SurfaceSet s -> m (SurfaceSet s))
+          -> (Surface s -> m (Surface s))
+          -> SurfaceSet s
+          -> m (SurfaceSet s)
 modifySet branchFunc nodeFunc (STree l n r) =
     STree <$> mapM branchFunc l <*> nodeFunc n <*> mapM branchFunc r
 
 -- | Commits a single surface.
-commitSingle :: SurfaceMonad m
-             => Bool        -- ^ Whether the surface state should be updated.
-             -> Bool        -- ^ Whether the backend should be updated.
-             -> Surface     -- ^ The surface to update.
-             -> m Surface   -- ^ A computation returning the new surface
+commitSingle :: SurfaceMonad m s
+             => Bool          -- ^ Whether the surface state should be updated.
+             -> Bool          -- ^ Whether the backend should be updated.
+             -> Surface s     -- ^ The surface to update.
+             -> m (Surface s) -- ^ A computation returning the new surface
 commitSingle updateSurface updateBackend surf =
     let f = if updateSurface then commitState   else id
         g = if updateBackend then commitBackend else return
@@ -168,7 +175,7 @@ commitSingle updateSurface updateBackend surf =
 -- | Commits a child sub-tree.
 --
 -- Only subtrees where the root surface has the sync-flag set is committed.
-commitChildren :: (SurfaceMonad m, Applicative m) => Bool -> SurfaceSet -> m SurfaceSet
+commitChildren :: (SurfaceMonad m s, Applicative m) => Bool -> SurfaceSet s -> m (SurfaceSet s)
 commitChildren root set
     | root && not (surfSync (label set)) = pure set
     | otherwise                          = prune . shuffle <$> modifySet (commitChildren False) (commitSingle False True) set
@@ -178,7 +185,7 @@ commitChildren root set
 -- If the root of the subtree is in sync-mode, only the root surface is
 -- updated. If it is desync-mode, subtrees where the root-surface has the
 -- sync-flag set is committed as well.
-commitRoot :: (SurfaceMonad m, Applicative m) => Bool -> SurfaceSet -> m SurfaceSet
+commitRoot :: (SurfaceMonad m s, Applicative m) => Bool -> SurfaceSet s -> m (SurfaceSet s)
 commitRoot sync' set =
     f <$> modifySet c (commitSingle True $ not sync) set
     where
@@ -189,29 +196,31 @@ commitRoot sync' set =
 
 -- | Commits all subsurfaces in a subtree until it hits a subsurface with the
 -- sync-flag set.
-commitSync :: (SurfaceMonad m, Applicative m) => SurfaceSet -> m SurfaceSet
+commitSync :: (SurfaceMonad m s, Applicative m) => SurfaceSet s -> m (SurfaceSet s)
 commitSync set
     | surfSync (label set) = pure set
     | otherwise            = modifySet commitSync (commitSingle False True) set
 
 -- | Checks if the current node inherits the sync flag for any of its parents.
-inheritsSync :: Zipper Surface -> Bool
+inheritsSync :: Zipper (Surface s) -> Bool
 inheritsSync = any (surfSync . label . getTree) . parents
 
 -- | Searches for a specific surface in a surface set and returns a zipper to it.
-findSid :: SurfaceId -> SurfaceSet -> Maybe (Zipper Surface)
+findSid :: SurfaceId -> SurfaceSet s -> Maybe (Zipper (Surface s))
 findSid sid = findFirst ((\s -> surfId s == sid && not (surfDeleted s)) . label)
 
 -- | Searches for a specific surface in surface set, then applies a function to
 -- the subtree it is the parent of.
-modifySid :: (Applicative m, Monad m) => (Bool -> SurfaceSet -> m SurfaceSet) -> SurfaceId -> SurfaceSet -> m SurfaceSet
+modifySid :: (Applicative m, Monad m)
+          => (Bool -> SurfaceSet s -> m (SurfaceSet s))
+          -> SurfaceId -> SurfaceSet s -> m (SurfaceSet s)
 modifySid func sid set =
     case findFirst ((== sid ) . surfId . label) set of
          Nothing -> pure set
          Just z  -> toTree <$> modifyA (func $ inheritsSync z) z
 
 -- | Commits a surface, and possibly its subsurfaces.
-commit :: (SurfaceMonad m, Applicative m) => SurfaceId -> SurfaceSet -> m SurfaceSet
+commit :: (SurfaceMonad m s, Applicative m) => SurfaceId -> SurfaceSet s -> m (SurfaceSet s)
 commit = modifySid commitRoot
 
 -- | Updates the sync value of a surface.
@@ -219,7 +228,7 @@ commit = modifySid commitRoot
 -- If the surface is the top-most surface with the sync-flag set, and the flag
 -- is disabled, all surface below it without the sync-flag set must be
 -- committed.
-setSync :: (SurfaceMonad m, Applicative m) => Bool -> SurfaceId -> SurfaceSet -> m SurfaceSet
+setSync :: (SurfaceMonad m s, Applicative m) => Bool -> SurfaceId -> SurfaceSet s -> m (SurfaceSet s)
 setSync newVal = modifySid updateSync
     where
         updateSync sync curSet@(STree l n r)
@@ -236,7 +245,7 @@ setSync newVal = modifySid updateSync
 --
 -- If they are siblings, the common root is their parent. If they are
 -- parent-child, the parent is the common root.
-findCommonRoot :: SurfaceId -> SurfaceId -> SurfaceSet -> Maybe (Zipper Surface)
+findCommonRoot :: SurfaceId -> SurfaceId -> SurfaceSet s -> Maybe (Zipper (Surface s))
 findCommonRoot a b set = do
     guard (a /= b)
 
@@ -255,19 +264,19 @@ findCommonRoot a b set = do
     return root
 
 -- | Adds a shuffle command to the root of a surface set.
-addShuffle :: Shuffle -> SurfaceSet -> SurfaceSet
+addShuffle :: Shuffle -> SurfaceSet s -> SurfaceSet s
 addShuffle s (STree l n r) = STree l (n { surfShuffle = s : surfShuffle n}) r
 
 -- | Places a surface above another surface.
-placeAbove :: SurfaceId -> SurfaceId -> SurfaceSet -> Maybe SurfaceSet
+placeAbove :: SurfaceId -> SurfaceId -> SurfaceSet s -> Maybe (SurfaceSet s)
 placeAbove a b = fmap (toTree . modify (addShuffle $ PlaceAbove a b)) . findCommonRoot a b
 
 -- | Places a surface below another surface.
-placeBelow :: SurfaceId -> SurfaceId -> SurfaceSet -> Maybe SurfaceSet
+placeBelow :: SurfaceId -> SurfaceId -> SurfaceSet s -> Maybe (SurfaceSet s)
 placeBelow a b = fmap (toTree . modify (addShuffle $ PlaceBelow a b)) . findCommonRoot a b
 
 -- | Removes a surface from a surface set.
-delete :: SurfaceId -> SurfaceSet -> Maybe SurfaceSet
+delete :: SurfaceId -> SurfaceSet s -> Maybe (SurfaceSet s)
 delete sid = fmap (toTree . modify f) . findSid sid
     where
         f (STree l n r) = STree l (n { surfDeleted = True }) r

9664d5c4e850c72df7186ffe43ea6a4a7c4fd222
Sivert Berg <code@trev.is>
2015-05-11 21:38:41 +0200

    Make STree an instance of Foldable and Traversable

--- src/Data/STree.hs
+++ src/Data/STree.hs
@@ -5,11 +5,26 @@ module Data.STree
     )
 where
 
+import Control.Applicative
+import Data.Foldable
+import Data.Monoid
+import Data.Traversable
+
 data STree a = STree [STree a] a [STree a]
-    deriving (Eq)
+    deriving (Eq, Show)
 
 instance Functor STree where
     fmap f (STree l n r) = STree (fmap (fmap f) l) (f n) (fmap (fmap f) r)
+
+instance Foldable STree where
+    foldMap f (STree l n r) = mconcat $ map (foldMap f) l ++ [f n] ++ map (foldMap f) r
+
+instance Traversable STree where
+    traverse f (STree l n r) =
+        STree
+        <$> traverse (traverse f) l
+        <*> f n
+        <*> traverse (traverse f) r
 
 -- | Return the label value.
 label :: STree a -> a

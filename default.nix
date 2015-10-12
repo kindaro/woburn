3de4df12413c59f6616cd51b9a64a0b6d7cb1fa2
Sivert Berg <code@trev.is>
2015-10-12 22:07:21 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,20 @@
-{ cabal, cabalInstall_1_20_0_6, filterSource, QuickCheck, waylandWire, linear }:
-
-cabal.mkDerivation
-( self:
-  { pname = "woburn"
-  ; version = "0.1.0"
-  ; src = filterSource ./.
-  ; buildTools = [ cabalInstall_1_20_0_6 ]
-  ; buildDepends = [ waylandWire linear ]
-  ; testDepends = [ QuickCheck ]
-  ; doCheck = true
-  ; enableSplitObjs = false
-  ;
-  }
-)
+{ mkDerivation, base, containers, diet-set, linear, mtl, QuickCheck, pipes, pipes-concurrency, async
+, stdenv, wayland-wire, vim, cabal-install, wayland, pkgconfig, hlint
+, git, less
+}:
+mkDerivation {
+  pname = "woburn";
+  version = "0.1.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildTools = [ vim cabal-install pkgconfig hlint git less ];
+  buildDepends = [
+    base containers diet-set linear mtl wayland-wire wayland pipes pipes-concurrency async
+  ];
+  testDepends = [
+    base containers diet-set linear mtl QuickCheck wayland-wire pipes pipes-concurrency async
+  ];
+  description = "Wayland compositor";
+  license = stdenv.lib.licenses.gpl3;
+}

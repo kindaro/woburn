38399648abddea495a91dc657cb1dfa23768939e
Sivert Berg <code@trev.is>
2015-12-06 18:33:19 +0100

    Implement dummy layout algorithm that makes all windows fullscreen

--- src/Woburn/Layout.hs
+++ src/Woburn/Layout.hs
@@ -1,10 +1,15 @@
 module Woburn.Layout
 where
 
+import Control.Arrow
 import Data.Rect
 import Data.Word
+import qualified Data.List.Zipper as Z
 import Woburn.Output
 import Woburn.Universe
 
 layout :: Universe a -> [(MappedOutput, [(Rect Word32, a)])]
-layout = undefined
+layout = map (output &&& layoutScreen) . Z.toList . screens
+
+layoutScreen :: Screen a -> [(Rect Word32, a)]
+layoutScreen s = map ((,) (mappedRect $ output s)) . Z.toList . windows $ workspace s

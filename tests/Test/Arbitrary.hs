3b6e1171255170cd85fd370d8d7efb73f2965e4d
Sivert Berg <code@trev.is>
2016-01-07 20:29:50 +0100

    Speed up tests using STree

--- tests/Test/Arbitrary.hs
+++ tests/Test/Arbitrary.hs
@@ -4,10 +4,10 @@ where
 
 import Control.Applicative
 import Control.Arrow
-import Data.Foldable (toList)
-import Data.List (nub)
+import Control.Monad.State
 import Data.STree
 import Data.Word
+import Prelude
 import Woburn.Surface
 import Test.QuickCheck
 
@@ -15,18 +15,20 @@ instance Arbitrary SurfaceId where
     arbitrary = fromIntegral <$> (arbitrary :: Gen Word32)
     shrink s = fromIntegral <$> shrink (fromIntegral s :: Word32)
 
-instance (Eq a, Arbitrary a) => Arbitrary (STree a) where
-    arbitrary = suchThat (sized f) (uncurry (==) . (id &&& nub) . toList)
+instance (Num a) => Arbitrary (STree a) where
+    arbitrary = sized ((`evalStateT` 0) . f)
         where
-            f 0 = STree <$> pure [] <*> arbitrary <*> pure []
-            f n = do
-                l <- choose (0, n)
-                r <- choose (0, n - l)
-                let n' = n `div` (l + r)
+            nextElem = state (id &&& (+ 1))
+            f n
+              | n <= 1    = STree <$> pure [] <*> nextElem <*> pure []
+              | otherwise = do
+                l <- lift $ choose (0, n - 1)
+                r <- lift $ choose (0, n - 1 - l)
+                let n' = (n - 1) `div` (l + r)
                 STree
-                    <$> vectorOf l (f n')
-                    <*> arbitrary
-                    <*> vectorOf r (f n')
+                    <$> replicateM l (f n')
+                    <*> nextElem
+                    <*> replicateM r (f n')
 
     shrink (STree [] _ []) = []
     shrink (STree l  n r ) =

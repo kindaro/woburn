bca582ee4ac659d0a3ffc54bf40eb3ad45a61de6
Sivert Berg <code@trev.is>
2016-02-11 21:45:51 +0100

    Fix bug and simplify Woburn.hs

--- src/Woburn.hs
+++ src/Woburn.hs
@@ -52,26 +52,24 @@ makeMunmapFinalizer = do
               Just size -> void $ c'munmap (castPtr ptr) size
 
 -- | Runs a single client.
-runClient :: FinalizerPtr Word8
-          -> MVar (M.Map (Ptr Word8) CSize)
-          -> ClientId
-          -> Socket
-          -> RMChan Event
-          -> WMChan Request
+runClient :: FinalizerPtr Word8             -- ^ A finalizer for mmap'ed memory
+          -> MVar (M.Map (Ptr Word8) CSize) -- ^ Data used by the mmap finalizer
+          -> ClientId                       -- ^ The client ID of this client.
+          -> Socket                         -- ^ The client socket.
+          -> Chan (Either Message Event)    -- ^ The channel new events will arrive on.
+          -> WMChan (CoreInput s)           -- ^ The channel core requests are sent on.
           -> IO ()
-runClient munmapFinalizer finalizerData cid sock rEvt wReq = do
-    chan             <- newChan
-    ea               <- async . readUntilClosed rEvt $ writeChan chan . Right
+runClient munmapFinalizer finalizerData cid sock chan reqWr = do
     ((_, iMgr), iFs) <- foldF interpretFrontend (runFrontend initFrontend newObjectManager initialFrontendState)
-    link ea
 
-    forever . (`evalStateT` (iMgr, iFs))  $ do
+    (`evalStateT` (iMgr, iFs)) . forever  $ do
         (mgr, fs) <- get
         sa        <- liftIO . async $ recv (messageLookup mgr) sock >>= writeChan chan . Left
         liftIO $ link sa
 
         m         <- liftIO $ readChan chan
         liftIO $ cancel sa
+        liftIO $ putStrLn ("<< " ++ show m)
 
         let f = case m of
                   Left  msg -> handleMessage msg
@@ -82,10 +80,19 @@ runClient munmapFinalizer finalizerData cid sock rEvt wReq = do
         put (mgr', fs')
         liftIO $ logError res
     where
-        interpretFrontend (SendMessage msg a       ) = send sock msg >> return a
-        interpretFrontend (SendRequest req a       ) = writeMChan wReq req >> return a
         interpretFrontend (GetClientId f           ) = return $ f cid
         interpretFrontend (GetTimestamp f          ) = (f . round . (* 1000)) <$> getPOSIXTime
+
+        interpretFrontend (SendMessage msg a       ) = do
+            putStrLn (">> " ++ show msg)
+            send sock msg
+            return a
+
+        interpretFrontend (SendRequest req a       ) = do
+            putStrLn (">> " ++ show req)
+            writeMChan reqWr (ClientRequest cid req)
+            return a
+
         interpretFrontend (MapMemory (Fd fd) size f) = do
             let cSize = fromIntegral size
             ptr <- c'mmap nullPtr cSize c'PROT_READ c'MAP_SHARED fd 0
@@ -101,72 +108,68 @@ logError :: Either ObjectError () -> IO ()
 logError = either (hPrint stderr) return
 
 -- | Waits for incoming client connections, and spins of new threads to run them.
-waitForClients :: FinalizerPtr Word8                              -- ^ A finalizer that calls munmap.
-               -> MVar (M.Map (Ptr Word8) CSize)                  -- ^ Data used by the finalizer.
-               -> MVar (D.Diet ClientId)                          -- ^ A set of free 'ClientId's
-               -> WMChan (ClientId, WMChan Event, RMChan Request) -- ^ The channel new clients will be announced on.
-               -> Socket                                          -- ^ The socket to listen on.
+waitForClients :: FinalizerPtr Word8                                  -- ^ A finalizer that calls munmap.
+               -> MVar (M.Map (Ptr Word8) CSize)                      -- ^ Data used by the finalizer.
+               -> MVar (D.Diet ClientId)                              -- ^ A set of free 'ClientId's
+               -> MVar (M.Map ClientId (Chan (Either Message Event))) -- ^ A map of client's event channels.
+               -> WMChan (CoreInput s)                                -- ^ The channel core requests are sent on.
+               -> Socket                                              -- ^ The socket to listen on.
                -> IO ()
-waitForClients munmapFinalizer finalizerData clientIds wChan socket = forever $ do
+waitForClients munmapFinalizer finalizerData clientIds clientsVar wChan socket = forever $ do
     clientSocket <- accept socket
     cid          <- modifyMVar clientIds (return . swap . fromMaybe (error "Ran out of client IDs") . D.minView)
-    (rEvt, wEvt) <- newMChan
-    (rReq, wReq) <- newMChan
+    evtChan      <- newChan
+
+    modifyMVar_ clientsVar (return . M.insert cid evtChan)
+    writeMChan wChan $ ClientAdd cid
+
     void .
         forkIO $
-        finally
-        (runClient munmapFinalizer finalizerData cid clientSocket rEvt wReq)
-        (close clientSocket >> closeMChan wReq >> modifyMVar_ clientIds (return . D.insert cid))
+        runClient munmapFinalizer finalizerData cid clientSocket evtChan wChan
+        `finally`
+        finalizer cid clientSocket
 
-    writeMChan wChan (cid, wEvt, rReq)
-
--- | Handles events and requests going to and from the different clients.
-clientManager :: RMChan (ClientId, WMChan Event, RMChan Request)
-              -> WMChan (CoreInput s)
-              -> Chan (Maybe ClientId, Event)
-              -> IO ()
-clientManager newClients coreInp clientEvts =
-    readUntilClosed newClients newClient
     where
-        newClient (cid, wEvt, rReq) = do
-            evtReader <- async $ do
-                chan <- dupChan clientEvts
-                forever $ do
-                    (mCid, evt) <- readChan chan
-                    when (maybe True (== cid) mCid) $ writeMChan wEvt evt
+        finalizer cid sock = do
+            close sock
+            modifyMVar_ clientIds (return . D.insert cid)
+            modifyMVar_ clientsVar (return . M.delete cid)
+            writeMChan wChan $ ClientDel cid
 
-            reqWriter <- async $ do
-                writeMChan coreInp (ClientAdd cid)
-                readUntilClosed rReq  $ writeMChan coreInp . ClientRequest cid
-                cancel evtReader
-                closeMChan wEvt
-                writeMChan coreInp (ClientDel cid)
-
-            link evtReader
-            link reqWriter
+-- | Dispatches events from core to the correct clients.
+eventDispatcher :: MVar (M.Map ClientId (Chan (Either a Event)))
+                -> Chan (Maybe ClientId, Event)
+                -> IO ()
+eventDispatcher clientsVar clientEvts = forever $ do
+    (mCid, evt) <- readChan clientEvts
+    withMVar clientsVar $ \clients ->
+        case mCid of
+          Nothing  -> mapM_ (`writeChan` Right evt) (M.elems clients)
+          Just cid -> maybe (return ()) (`writeChan` Right evt) (M.lookup cid clients)
 
 -- | Starts Woburn.
 --
 -- Starts listening for new clients on the given path, or if the path is
 -- 'Nothing' it default to the value of the "WAYLAND_DISPLAY" environment
 -- variable. If that one does not exist it will use
--- "$XDG_RUNTIME_DIR/wayland-0" or "/tmp/wayland-0".
+-- "$XDG_RUNTIME_DIR/wayland-0", or "/tmp/wayland-0" if $XDG_RUNTIME_DIR is not
+-- set.
 run :: Maybe String -> IO ()
 run path = do
     clientEvts                 <- newChan
     (inpRd, inpWr)             <- newMChan
-    (clientRd, clientWr)       <- newMChan
     (bReqWr, bEvtRd, surfGet)  <- gtkBackend
     clientIds                  <- newMVar . D.singletonI $ D.Interval minBound maxBound
+    clientsVar                 <- newMVar M.empty
     (finalizer, finalizerData) <- makeMunmapFinalizer
 
     bracket (listen path) close $ \sock -> do
-        wc <- async (waitForClients finalizer finalizerData clientIds clientWr sock)
+        wc <- async (waitForClients finalizer finalizerData clientIds clientsVar inpWr sock)
         be <- async (readUntilClosed bEvtRd (writeMChan inpWr . BackendEvent))
-        cm <- async (clientManager clientRd inpWr clientEvts)
-        mapM_ link [wc, be, cm]
+        ed <- async (eventDispatcher clientsVar clientEvts)
+        mapM_ link [wc, be, ed]
         evalStateT (readUntilClosed inpRd (runCore surfGet bReqWr clientEvts)) newCoreState
-        mapM_ cancel [wc, be, cm]
+        mapM_ cancel [wc, be, ed]
     where
         runCore surfGet reqWr evtWr inp =
             StateT $ foldF (interpretCore surfGet reqWr evtWr) . runStateT (handleInput inp)
@@ -175,5 +178,5 @@ run path = do
             case inp of
               ClientEvent cid evt a -> writeChan evtWr (cid, evt) >> return a
               BackendRequest  req a -> writeMChan reqWr req >> return a
-              BackendSurfGet      f -> f <$> liftIO surfGet
-              CoreError       err _ -> error err
+              BackendSurfGet      f -> f <$> surfGet
+              CoreError       err a -> hPutStrLn stderr ("CoreError: " ++ err) >> return a

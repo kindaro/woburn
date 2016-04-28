fedcd91d59b21cf84a4c64cc2f59abf13153d447
Sivert Berg <code@trev.is>
2016-04-28 20:35:27 +0200

    Add profiling option to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
-{ haskellPackages ? (import <nixpkgs>{}).haskellPackages }:
+{ profiling ? false }:
 let
+  orignalPackages = (import <nixpkgs>{}).haskellPackages;
+  haskellPackages =
+  if profiling
+  then orignalPackages.override {
+    overrides = self : super: { mkDerivation = drv : super.mkDerivation (drv // {enableLibraryProfiling = true;});
+    };
+  }
+  else orignalPackages;
   all = (import ../../nix-packages.nix) { haskellPackages = haskellPackages; isShell = true; };
 in
 all.woburn.env
